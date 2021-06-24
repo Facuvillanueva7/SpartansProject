2823db252251c877b1accb57145596521b2dfdc2
Facuvillanueva7 <facuvillanueva1@gmail.com>
2021-06-23 22:12:39 -0300

    Getnoticiaindividual-solved

--- client/src/Components/Views/Noticia.js
+++ client/src/Components/Views/Noticia.js
@@ -2,19 +2,33 @@ import React,{useState} from 'react'
 import { useParams } from 'react-router-dom';
 import {db} from '../../config/firebase'
 const Noticia = () => {
-    const  {id} = useParams()
-    const [noticia,setNoticia]= useState([]);
+    const  {id}    = useParams()
+   /*  const [noticia,setNoticia]= useState([]); */
+    const [Body, setBody]= useState("");
+    const [NoticiaImg, setNoticiaImg]= useState("")
     const getNoticia = async()=>{
             await db 
-           .collection("Noticias-general")
-           
-           .onSnapshot((querysnapshot)=>{
+           .collection("Noticias-general").doc(id)
+           .get()
+           .then((doc=>{
+               if (doc.exists){
+               /*     setNoticia(doc) */
+               setBody(doc.data().Body)
+               setNoticiaImg(doc.data().NoticiaImg)
+                   console.log("Document data:", Body, NoticiaImg);
+               } else {
+                   console.log("No matchs");
+               }
+           })).catch((error)=>{
+               console.log("Error getting document:", error);
+           })
+           /* .onSnapshot((querysnapshot)=>{
             const docs = [];
             querysnapshot.forEach((doc)=>{
                 docs.push({...doc.data(),id:doc.id})
             });
             setNoticia(docs)
-           })
+           }) */
                  
          /* console.log(noticia.data());  */
     }
@@ -26,7 +40,7 @@ const Noticia = () => {
         <div>
            
             <h2 onMouseOver={getNoticia}>Lorem ipsum dolor sit amet consectetur.</h2>
-            <h2>{id}</h2>
+           {/*  <h2>{id}</h2>
              {noticia.map((noticia)=>(
                  <div className="card-body" key={noticia.id}>
                      <div className="card-header">
@@ -34,7 +48,14 @@ const Noticia = () => {
                      </div>
                      <img src={noticia?.NoticiaImg}  style ={{width:"50%"}}alt="sample" />
                  </div>
-             ))}
+             ))} */}
+             {<>
+                 <h2>{Body}</h2>
+                 <div className="card-body">
+                    <img src={NoticiaImg} alt="sample" />
+                 </div>
+                 </>
+             }
         </div>
         </>
     )
