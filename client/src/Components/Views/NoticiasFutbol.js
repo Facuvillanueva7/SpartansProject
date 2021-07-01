1f7957ff13a6cc5699f4032382c3480ef5c4b16d
JuanJardin <juanchijardin96@gmail.com>
2021-07-01 17:44:07 -0300

    noticia futbol individual con diseño

--- client/src/Components/Views/NoticiasFutbol.js
+++ client/src/Components/Views/NoticiasFutbol.js
@@ -1,53 +1,70 @@
-import React,{useEffect,useState} from 'react'
-import {db} from '../../config/firebase'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { db } from "../../config/firebase";
 const NoticiasFutbol = () => {
-    const [noticiasFutbol,setNoticiasFutbol] = useState([]);
-    const getNoticiasFutbol = async ()=>{
-        await db
-        .collection("Noticias-Futbol")
-        .orderBy("Date","desc")
-        .onSnapshot((querysnapshot)=>{
-            const docs = []
-            querysnapshot.forEach((doc)=>{
-                docs.push({...doc.data(),id:doc.id})
-            });
-            setNoticiasFutbol(docs)
+  const [noticiasFutbol, setNoticiasFutbol] = useState([]);
+  const [CurrentId, setCurrentId] = useState();
+  const getNoticiasFutbol = async () => {
+    await db
+      .collection("Noticias-Futbol")
+      .orderBy("Date", "desc")
+      .onSnapshot((querysnapshot) => {
+        const docs = [];
+        querysnapshot.forEach((doc) => {
+          docs.push({ ...doc.data(), id: doc.id });
         });
-    };
-    useEffect(()=>{
-        getNoticiasFutbol()
-    })
-    return (
-        <>
-        <div>
-          <div className="col-md-4 p-2">
-            {noticiasFutbol.map((noticia) => (
-              <div className="card-mb-1" key={noticia.id}>
-                <div className="card-body">
-                  <div className="d-flex justify-content-between">
-                    {noticia.NoticiaFutbolImg && (
-                      <img
-                        src={noticia?.NoticiaFutbolImg}
-                        style={{ width: "50%" }}
-                        alt="sample"
-                      />
-                    )}
-                    <h4>{noticia.Title}</h4>
-                    <div className="container-">
+        setNoticiasFutbol(docs);
+      });
+  };
+  useEffect(() => {
+    getNoticiasFutbol();
+  });
+  const getNoticiaFutbolIndividual = async () => {
+    try {
+      if (CurrentId) {
+        const data = await db
+          .collection("Noticias-futbol")
+          .doc(CurrentId)
+          .get();
+          console.log(data.id)
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <>
+      <div>
+        <div className="col-md-4 p-2">
+          {noticiasFutbol.map((noticia) => (
+            <div className="card-mb-1" key={noticia.id}>
+              <div className="card-body">
+                <div className="d-flex justify-content-between">
+                  {noticia.NoticiaFutbolImg && (
+                    <img
+                      src={noticia?.NoticiaFutbolImg}
+                      style={{ width: "50%" }}
+                      alt="sample"
+                      onClick={getNoticiaFutbolIndividual}
+                    />
+                  )}
+                  <Link to={"./noticiafutbol/" + noticia.id}>
+                    <h4 onClick={() =>setCurrentId}>{noticia.Title}</h4>
+
                     <p>{noticia.Body}</p>
                     <p>{noticia.Copete}</p>
                     <p>{noticia.Description}</p>
                     <p>{noticia.Fuente}</p>
                     <p>{noticia.Fecha}</p>
-                    </div>
-                  </div>
+                  </Link>
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
-      </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default NoticiasFutbol
+export default NoticiasFutbol;
