31e280e8795c48b8f017f8325e894ac084436946
JuanJardin <juanchijardin96@gmail.com>
2021-07-12 17:52:23 -0300

    Header.js eliminado

--- client/src/Components/Views/PartidoBasket.js
+++ client/src/Components/Views/PartidoBasket.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { db } from "../../config/firebase";
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { NavLink, Link } from "react-router-dom";
-import Header from '../Views/Header'
+import Background from "../../assets/img/Basquebolista-mujer.jpg";
 
 //CSS
 import "../../assets/bootstrap/css/bootstrap.min.css";
@@ -90,7 +90,55 @@ const PartidoBasket = () => {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Header/>
+      <header style={{ marginLeft: "-11px", fontSize: "35px" }}>
+        <div
+          className="jumbotron jumbotron-fluid"
+          style={{
+            // backgroundImage: `(${Background})`,
+            // backgroundPosition: "center",
+            //minWidth: "360px",
+            marginRight: "-22px",
+            // paddingBottom: "1px",
+            backgroundColor: "rgba(26,26,26)",
+            // overflow: "hidden",
+            //height: "80%"
+          }}
+        >
+          <div>
+            <h1
+              className="text-light"
+              style={{
+                paddingTop: "40px",
+                position: "absolute",
+                zIndex: "2",
+                marginLeft: "30px",
+                fontSize: "60px"
+              }}
+            >
+              Titulo Noticia
+            </h1>
+            <p
+              className="text-white"
+              style={{
+                marginTop: "120px",
+                marginLeft: "30px",
+                position: "absolute",
+                zIndex: "3",
+                
+              }}
+            >
+              Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo
+              odio, dapibus ac facilisis in, egestas eget quam.
+            </p>
+            <img
+              className="img-fluid"
+              src={Background}
+              style={{ position: "relative", zIndex: "1", width: "100%" }}
+              alt="Partidos Basket"
+            />
+          </div>
+        </div>
+      </header>
       <div className="container" style={{}}>
         <div className="row">
           <div className="col-md-12">
