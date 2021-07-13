9f8ee349369113989909ac95724af18efbc35a21
Facuvillanueva7 <facuvillanueva1@gmail.com>
2021-07-12 21:55:29 -0300

    noticias-basket-two-images-solved

--- client/src/Components/dashboard/NoticiasBasket/NoticiaBasketForm.js
+++ client/src/Components/dashboard/NoticiasBasket/NoticiaBasketForm.js
@@ -11,6 +11,9 @@ const NoticiaBasketlForm = (props) => {
   };
   const [values, setValues] = useState(initialStateValues);
   const [file, setFile] = useState();
+  const [file2,setFile2] = useState();
+  const [imgA,setImgA]= useState();
+  const [imgB,setImgB] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const types = ["image/png", "image/jpeg"];
   const noticiaBasketImgHandler = (e) => {
@@ -22,10 +25,64 @@ const NoticiaBasketlForm = (props) => {
       throw new Error("Incorrect_file_type");
     }
   };
+  const noticiaBasketImg2Handler = (e)=>{ 
+    let selectedFile2 = e.target.files[0];
+    if (selectedFile2 && types.includes(selectedFile2.type)) {
+      setFile2(selectedFile2);
+    } else {
+      setFile2(null);
+      throw new Error("incorrect_file_type");
+    }
+  }
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     const obj = values;
     obj[name] = value;
+  };
+  const uploadImgA = () => {
+    const NoticiaBasketImg = storage
+      .ref(`Noticias-Basket-images/${file.name}`)
+      .put(file);
+    NoticiaBasketImg.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setIsLoading(progress !== 100);
+      },
+      () => {},
+      async () => {
+        const url = await storage
+          .ref("Noticias-Basket-images")
+          .child(file.name)
+          .getDownloadURL();
+        setFile(url);
+        setImgA(url);
+        console.log(url);
+      }
+    );
+  };
+  const uploadImgB = () => {
+    const NoticiaBasketImg2 = storage
+      .ref(`Noticias-Basket-images-B/${file2.name}`)
+      .put(file2);
+    NoticiaBasketImg2.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setIsLoading(progress !== 100);
+      },
+      () => {},
+      async () => {
+        const url2 = await storage
+          .ref("Noticias-Basket-images-B")
+          .child(file2.name)
+          .getDownloadURL();
+        setImgB(url2);
+        console.log(url2);
+      }
+    );
   };
   const addOrEditNoticiaBasket = async (noticiaBasketObject) => {
     try {
@@ -46,27 +103,15 @@ const NoticiaBasketlForm = (props) => {
     }
     setIsLoading(false);
   };
+  const submitNoticiaBasket = async () => {
+    if(!imgA && !imgB) {
+      console.log("No se puede subir la noticia aún");
+    } else {
+      await addOrEditNoticiaBasket ({...values,imgA,imgB})
+    }
+  }
   const handleSubmit = (e) => {
     e.preventDefault();
-    const NoticiaBasketImg = storage
-      .ref(`Noticias-Basket-images/${file.name}`)
-      .put(file);
-    NoticiaBasketImg.on(
-      "state_changed",
-      (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setIsLoading(progress !== 100);
-      },
-      () => {},
-      async () => {
-        const url = await storage
-          .ref("Noticias-Basket-images")
-          .child(file.name)
-          .getDownloadURL();
-        addOrEditNoticiaBasket({ ...values, NoticiaBasketImg: url });
-      }
-    );
   };
   const getNoticiaBasketById = async (id) => {
     if (!id) return;
@@ -141,8 +186,19 @@ const NoticiaBasketlForm = (props) => {
             placeholder="Sube una imagen"
             onChange={noticiaBasketImgHandler}
           />
-
-          <button className="btn btn-primary btn-block" disabled={isLoading}>
+          <button className="btn btn-success mb-1" disabled={isLoading} onClick={uploadImgA}>
+            Confirmar imagen 1
+          </button>
+          
+          <input
+            type="file"
+            placeholder="Sube una imagen"
+            onChange={noticiaBasketImg2Handler}
+          />
+          <button className="btn btn-success mb-1" disabled={isLoading} onClick={uploadImgB}>
+            Confirmar imagen 2
+          </button>
+          <button className="btn btn-primary btn-block" disabled={isLoading} onClick={submitNoticiaBasket}>
             {props.currentId === "" ? "Save" : "Update"}
           </button>
         </form>
